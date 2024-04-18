9151fc7a3dfa77599fecffc428b4707ab4db00ee
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-04-18 19:45:21 -0300

    Usuário Dono de Buffet se autentica com sucesso

---
+++ spec/system/authentication/buffet_owner_user_sign_in_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Usuário Dono de Buffet se autentica' do
+  it 'com sucesso' do
+    BuffetOwner.create!(
+      email: 'support@wolfgangpuck.com', 
+      password: 'biE@u4&mZ5G3p3')
+
+    visit root_path
+    click_on 'Faça seu Login'
+    within('main') do
+      fill_in 'E-mail', with: 'support@wolfgangpuck.com'
+      fill_in 'Senha', with: 'biE@u4&mZ5G3p3'
+      click_on 'Entrar'
+    end
+
+    expect(page).to have_content 'Login efetuado com sucesso.'
+    within('nav') do
+      expect(page).to have_button 'Sair'
+      expect(page).not_to have_link 'Faça seu Login'
+      expect(page).to have_content 'support@wolfgangpuck.com'
+    end
+  end
+end
