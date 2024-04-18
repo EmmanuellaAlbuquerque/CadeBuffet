23ab9bbd36020b58a5af9536b16325d51097fa3c
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-04-18 19:47:17 -0300

    Usuário Dono de Buffet se autentica e faz logout

--- spec/system/authentication/buffet_owner_user_sign_in_spec.rb
+++ spec/system/authentication/buffet_owner_user_sign_in_spec.rb
@@ -21,4 +21,26 @@ describe 'Usuário Dono de Buffet se autentica' do
       expect(page).to have_content 'support@wolfgangpuck.com'
     end
   end
+
+  it 'e faz logout' do
+    BuffetOwner.create!(
+      email: 'support@wolfgangpuck.com', 
+      password: 'biE@u4&mZ5G3p3')
+
+    visit root_path
+    click_on 'Faça seu Login'
+    within('main') do
+      fill_in 'E-mail', with: 'support@wolfgangpuck.com'
+      fill_in 'Senha', with: 'biE@u4&mZ5G3p3'
+      click_on 'Entrar'
+    end
+    click_on 'Sair'
+
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    within('nav') do
+      expect(page).to have_link 'Faça seu Login'
+      expect(page).not_to have_button 'Sair'
+      expect(page).not_to have_content 'support@wolfgangpuck.com'
+    end        
+  end  
 end
