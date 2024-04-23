af8ee996860c78d13511f9d78e36b4023cac2c8a
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-04-23 17:26:56 -0300

    Um usuário visitante não autenticado acessa a página inicial e vê todos os buffets cadastrados

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   def index
     if current_buffet_owner
       redirect_to owner_dashboard_path
-    end    
+    end
+
+    @buffets = Buffet.all
   end
 end
---
+++ spec/system/guest/guest_user_view_homepage_spec.rb
@@ -0,0 +1,81 @@
+require 'rails_helper'
+
+describe 'Um usuário visitante não autenticado acessa a página inicial' do
+  it 'e vê todos os buffets cadastrados' do
+
+    pix = PaymentMethod.create!(name: 'Pix')
+    valet_service = ServiceOption.create!(name: 'Serviço de Valet')
+
+    fernando_tulipas = BuffetOwner.create!(
+      email: 'contato@fernandotulipas.com', 
+      password: 'fernandodastulipas123'
+    )
+
+    caio_cozinha = BuffetOwner.create!(
+      email: 'contato@caiocozinha.com', 
+      password: 'caio123'
+    )
+
+    grenah_gastronomia = BuffetOwner.create!(
+      email: 'contato@grenahgastronomia.com', 
+      password: 'grenahgastronomia123'
+    )
+
+    tulipas_buffet = Buffet.create!(        
+      trading_name: 'Buffet Tulipas - Villa Valentim', 
+      company_name: 'Buffet Tulipas - Villa Valentim Ltda.',
+      registration_number: '12345678000123', 
+      phone: '1129663900', 
+      email: 'contato@buffettulipas.com.br', 
+      address: 'Rua Valentim Magalhães, 293',
+      neighborhood: 'Alto da Mooca',
+      state: 'SP', 
+      city: 'São Paulo', 
+      zipcode: '01234567',
+      description: 'O Buffet Tulipas tem a satisfação de realizar com sucesso, casamentos, festas de debutantes, eventos corporativos, aniversários e bodas. Nossos belíssimos espaços, localizados no Alto da Mooca, são o cenário perfeito para o seu evento.',
+      buffet_owner: fernando_tulipas,
+      payment_methods: [pix]
+    )
+
+    caio_cozinha_buffet = Buffet.create!(        
+      trading_name: 'Caio Cozinha & Eventos', 
+      company_name: 'Caio Cozinha & Eventos Ltda.',
+      registration_number: '92732949000102', 
+      phone: '7723633113', 
+      email: 'contato@caiocozinha.com', 
+      address: 'Rua Comendador Bernardo Catarino, 89',
+      neighborhood: 'Centro',
+      state: 'BA', 
+      city: 'Salvador', 
+      zipcode: '12903834',
+      description: 'O Buffet Caio Cozinha & Eventos traz ao seu evento uma proposta gastronômica de primeira linha, preparada e executada com todo carinho, cuidado e qualidade para seu grande dia.',
+      buffet_owner: caio_cozinha,
+      payment_methods: [pix]
+    )
+
+    grenah_buffet = Buffet.create!(        
+      trading_name: 'Buffet Espaço Grenah | Gastronomia', 
+      company_name: 'Buffet Espaço Grenah | Gastronomia Ltda.',
+      registration_number: '00401207000178', 
+      phone: '1430298587', 
+      email: 'contato@grenahgastronomia.com', 
+      address: 'Rua Azevedo Soares, 633',
+      neighborhood: 'Jardim Anália Franco',
+      state: 'SP', 
+      city: 'Sorocaba', 
+      zipcode: '03322000',
+      description: 'Os profissionais do buffet confeccionam pratos artesanais da alta gastronomia e que agradam a todos os paladares. Para cada evento é preparado um menu personalizado, que reflita as preferências do anfitriões, mas que conquiste a todos os convidados.',
+      buffet_owner: grenah_gastronomia,
+      payment_methods: [pix]
+    )
+
+    visit root_path
+
+    expect(page).to have_link 'Buffet Tulipas - Villa Valentim'
+    expect(page).to have_content 'São Paulo - SP'
+    expect(page).to have_link 'Caio Cozinha & Eventos'
+    expect(page).to have_content 'Salvador - BA'
+    expect(page).to have_link 'Buffet Espaço Grenah | Gastronomia'
+    expect(page).to have_content 'Sorocaba - SP'
+  end
+end
